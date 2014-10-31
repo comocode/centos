50c11ed052e7262059fa6c8f5a52645d0df62cbd
Rulian Estivalletti <=>
2014-10-31 11:24:00 -0400

    excluding git upgrade by default

--- provision/shell/bootstrap.sh
+++ provision/shell/bootstrap.sh
@@ -2,5 +2,5 @@
 chmod +x /vagrant/provision/permissions/selinux.sh && sudo /vagrant/provision/permissions/selinux.sh
 
 #Upgrade o Git para 1.9.3 -- 
-chmod +x /vagrant/provision/git/latest.sh&&  sudo /vagrant/provision/git/latest.sh
+# chmod +x /vagrant/provision/git/latest.sh&&  sudo /vagrant/provision/git/latest.sh
 
